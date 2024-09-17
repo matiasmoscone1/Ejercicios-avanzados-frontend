fcb3d10c563992311670a2f568a65c0ed188879c
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-17 09:23:00 -0300

    Creando reducer

--- ejercicio-2/src/components/CartContextProvider.jsx
+++ ejercicio-2/src/components/CartContextProvider.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import { createContext } from "react";
 
 export const CartContext = createContext();
 
 const CartContextProvider = ({ children }) => {
 
+    const [cart, setCart] = useState([]);
 
+    
 
-
-    return(<CartContext.Provider value={{}}>
+    return(<CartContext.Provider value={{ cart }}>
         { children }
     </CartContext.Provider>);
 
---
+++ ejercicio-2/src/reducer.js
@@ -0,0 +1,20 @@
+
+
+const reducer = (state, action) => {
+
+    switch(state){
+        case "ADD_PRODUCT": {
+            break;
+        }
+        case "REMOVE_PRODUCT": {
+            break;
+        }
+        case "UPDATE_QUANTITY": {
+            break;
+        }
+        case "CLEAR_CART": {
+            break;
+        }
+    }
+
+}
