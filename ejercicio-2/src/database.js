23d0b1ec7fd094de52b35f8816edd869a88e1e75
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-17 09:52:42 -0300

    Armando la database

---
+++ ejercicio-2/src/database.js
@@ -0,0 +1,116 @@
+const dbProducts = [
+    {
+        id: 1,
+        article: "Laptop",
+        price: 300,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 2,
+        article: "Celular",
+        price: 110,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 3,
+        article: "Teclado",
+        price: 20,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 4,
+        article: "Mouse",
+        price: 25,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 5,
+        article: "Laptop",
+        price: 450,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 6,
+        article: "Monitor",
+        price: 130,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 7,
+        article: "Gabinete",
+        price: 30,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 8,
+        article: "Auriculares",
+        price: 60,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 9,
+        article: "Pendrive",
+        price: 15,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 10,
+        article: "Monitor",
+        price: 185,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 11,
+        article: "Mouse",
+        price: 35,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 12,
+        article: "Gabinete",
+        price: 25,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 13,
+        article: "Teclado",
+        price: 65,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 14,
+        article: "Celular",
+        price: 180,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 15,
+        article: "Cable USB",
+        price: 10,
+        image: "",
+        quantity: 1
+    },
+    {
+        id: 16,
+        article: "Celular",
+        price: 125,
+        image: "",
+        quantity: 1
+    }
+];
+
+export default dbProducts;
