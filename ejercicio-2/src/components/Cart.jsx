485b1614584e5170daf120b66c96f8a32710b9fb
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-18 14:06:10 -0300

    Creando modal para ver los productos del carrito

--- ejercicio-2/src/components/Cart.jsx
+++ ejercicio-2/src/components/Cart.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useContext } from "react";
 import { CartContext } from "./CartContextProvider";
 
@@ -5,19 +6,31 @@ import { CartContext } from "./CartContextProvider";
 const Cart = () => {
 
     const { cart } = useContext(CartContext);
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal);
+    }
+
 
     let acc = 0;
     cart.items.map((prod) => {
         acc += prod.price;
         console.log(acc);
     });
-    return(<div className="cart-container">
+    return(<><div className="cart-container">
         <div className="cart-info">
             <p>Productos: {cart.items.length}</p>
             <p>Precio total: ${acc}</p>
         </div>
-        <button className="btn-view-cart">View cart</button>
-    </div>);
+        <button className="btn-view-cart" onClick={() => toggleModal()}>View cart</button>
+    </div>
+    {modal && 
+        <div>
+            
+        </div>}
+    </>
+    );
 
 };
 
