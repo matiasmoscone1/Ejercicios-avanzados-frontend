f0469f5cac609640249ba3b8a972491f0c8f5e8a
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-09-20 17:16:07 -0300

    Problema de cantidades solucionado

--- ejercicio-2/src/reducer.js
+++ ejercicio-2/src/reducer.js
@@ -2,29 +2,20 @@
 const reducer = (state, action) => {
 
     switch(action.type){
-        case "ADD_PRODUCT": /*{
-            console.log(state);
-            state.items.map((prod) => {
-                if(prod.id === action.payload.id){
-                    prod.quantity += 1;
-                }else{
-                    return({...state, items: [...state.items, action.payload]});
-                }
-            });
-        }*/
-        {
+        case "ADD_PRODUCT": {
             const prodExist = state.items.find((prod) => prod.id === action.payload.id);
             
             if(prodExist){
                 return({...state, items: state.items.map((prod) => {
-                    prod.id === action.payload.id 
-                        ? {...prod, quantity: prod.quantity + 1 }
-                        : prod
-                })})
+                    if(prod.id === action.payload.id){
+                        return({...prod, quantity: prod.quantity + 1 })
+                    } else{
+                        return(prod);
+                    }
+                })});
             }else{
-                return({...state, items: [...state.items, action.payload]});
+                return({...state, items: [...state.items, {...action.payload, quantity: 1}]});
             }
-
         }
         case "REMOVE_PRODUCT": {
             return({...state, items: state.items.filter((item) => item.id !== action.payload)});
