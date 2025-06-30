c2561e29964d89dd2a31fcb784a1b980e9d632f7
matiasmoscone1 <matiasmoscone1@gmail.com>
2025-06-30 16:09:45 -0300

    Agregando algunas clases mas a tailwind

--- ejercicio-4 (TAILWIND)/src/components/Login.jsx
+++ ejercicio-4 (TAILWIND)/src/components/Login.jsx
@@ -8,15 +8,15 @@ const Login = () => {
 
 
     return(<>
-    <div>
-        <div className="h-[300px] w-xl flex flex-col justify-center items-center 
+    <div className="w-screen h-screen flex justify-center items-center">
+        <div className="h-[300px] w-xl flex flex-col justify-center items-center
         gap-8 m-auto border rounded-md">
             <div className="flex flex-col gap-2 w-md">
                 <input className="text-center border p-2 rounded-md" placeholder="Nombre de usuario" type="text"/>
                 <input className="text-center border p-2 rounded-md" placeholder="Contraseña" type="text"/>
             </div>
             <div className="flex flex-col w-md">
-                <button className="border p-2 rounded-md">Iniciar sesión</button>
+                <button className="p-2 rounded-md text-lg font-semibold text-white hover:text-black cursor-pointer bg-slate-500 hover:bg-slate-400">Iniciar sesión</button>
             </div>
         </div>
     </div>
