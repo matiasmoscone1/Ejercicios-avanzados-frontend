57340b104b0fb124a2eeefd401c02689187b6c13
matiasmoscone1 <matiasmoscone1@gmail.com>
2025-07-01 09:31:08 -0300

    Avanzando con el matcheo de usuario y contraseña

--- ejercicio-4 (TAILWIND)/src/components/Login.jsx
+++ ejercicio-4 (TAILWIND)/src/components/Login.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import { useContext } from "react";
 import { TaskContext } from "../context/TaskContext";
 
 
 const Login = () => {
 
-    const { state } = useContext(TaskContext);
+    const { state, users } = useContext(TaskContext);
+    const [userLog, setUserLog] = useState({});
+
+    const handleUser = (e) => {
+        setUserLog({...userLog, [e.target.name]: e.target.value});
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        if(userLog.user.match(users.username)){
+            console.log(users.username);
+        }    
     }
+
+    console.log(userLog);
 
     return(<>
     <div className="w-screen h-screen flex justify-center items-center">
@@ -16,8 +27,8 @@ const Login = () => {
             <div className="h-[300px] w-xl flex flex-col justify-center items-center
             gap-8 m-auto border rounded-md">
                 <div className="flex flex-col gap-3 w-md">
-                    <input className="text-center border p-2 rounded-md" placeholder="Nombre de usuario" type="text"/>
-                    <input className="text-center border p-2 rounded-md" placeholder="Contraseña" type="password"/>
+                    <input className="text-center border p-2 rounded-md" name="user" placeholder="Nombre de usuario" type="text" onChange={(e) => handleUser(e)}/>
+                    <input className="text-center border p-2 rounded-md" name="pass" placeholder="Contraseña" type="password" onChange={(e) => handleUser(e)}/>
                 </div>
                 <div className="flex flex-col w-md">
                     <button className="p-2 rounded-md text-lg font-semibold text-white hover:text-black cursor-pointer bg-slate-500 hover:bg-slate-400">Iniciar sesión</button>
