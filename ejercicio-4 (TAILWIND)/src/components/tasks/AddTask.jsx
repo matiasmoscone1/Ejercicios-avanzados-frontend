c608c4e3cb2d045b63ffaf85a47ec999fff3aa24
matiasmoscone1 <matiasmoscone1@gmail.com>
2025-07-07 08:12:49 -0300

    Acomodando estilos de boton de cerrado de popup

--- ejercicio-4 (TAILWIND)/src/components/tasks/AddTask.jsx
+++ ejercicio-4 (TAILWIND)/src/components/tasks/AddTask.jsx
@@ -8,17 +8,19 @@ const AddTask = () => {
 
     const { state, dispatch } = useContext(TaskContext);
     const popUpRef = useRef();
+    const closeBtnRef = useRef();
 
 
     const closePopUp = (e) => {
-        if(popUpRef.current && !popUpRef.current.contains(e.target)){
+        if(popUpRef.current && !popUpRef.current.contains(e.target) || closeBtnRef.current.contains(e.target)){
             dispatch({type: "CLOSE_POPUP", payload: false});
         }
     }
     
     return(
     <div className="fixed inset-0 flex justify-center items-center bg-black/50 z-50" onClick={(e) => closePopUp(e)}>
-        <div className="w-[70vw] h-[60vh] bg-white border rounded-md shadow-lg p-4" ref={popUpRef}>
+        <div className="w-[70vw] h-[60vh] bg-white border-gray-500 rounded-md shadow-lg p-4 relative" ref={popUpRef}>
+            <button className="w-5 rounded-md bg-red-500 text-white cursor-pointer absolute top-0 right-0" ref={closeBtnRef}>X</button>
 
         </div>
     </div>)
