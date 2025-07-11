663956dd61068af03d1ec3931b90ca571ed46733
matiasmoscone1 <matiasmoscone1@gmail.com>
2025-07-11 09:58:08 -0300

    Mejorando el renderizado de tareas de acuerdo a filtros

--- ejercicio-4 (TAILWIND)/src/components/filters/PopUpFilter.jsx
+++ ejercicio-4 (TAILWIND)/src/components/filters/PopUpFilter.jsx
@@ -21,7 +21,7 @@ const PopUpFilter = ( ) => {
         let newArray = [];
         switch(state.filterState.ref){
             case "Usuario":
-                newArray = state.tasks.filter((task) => task.user === state.filterState.user)
+                newArray = state.tasks.filter((task) => task.user === state.filterState.user);
                 setFilterArray(newArray);
                 break;
             case "Estado":
@@ -29,14 +29,14 @@ const PopUpFilter = ( ) => {
                 setFilterArray(newArray);
                 break;
             case "Fecha":
-                newArray = state.tasks.filter((task) => task.user === state.filterState.user)
+                newArray = state.tasks.filter((task) => task.date === state.filterState.date.split("-").reverse().join("/"));
                 setFilterArray(newArray);
                 break;
             default:
                 break;
         }
 
-    }, []);
+    }, [state.tasks, state.filterState]);
 
     console.log(filterArray);
 
@@ -44,10 +44,13 @@ const PopUpFilter = ( ) => {
         <div className="w-7/10 h-9/10 bg-white border border-gray-500 rounded-md shadow-lg p-4 relative" ref={popUpFilterRef}>
             <span className="p-4 text-lg block font-semibold text-center">Tareas filtradas por {state.filterState.ref}</span>
             {filterArray.map((task) => {
-                return(<div>
-                    {task.user}
-                    {task.state}
-                    </div>)
+                return(<div key={task.id} className="p-2 flex justify-center items-center gap-2 border-b">
+                <span className="w-20 truncate" title={task.user}>{task.user}</span>
+                <span className="text-sm w-50 truncate" title={task.title}>{task.title}</span>
+                <span className="text-sm w-100 truncate" title={task.description}>{task.description}</span>
+                <span>{task.date}</span>
+                <span>{task.state === "pendiente" ? <input type="checkbox" /> : <input type="checkbox" checked/> }</span>            
+            </div>)
             })}
 
         </div>
