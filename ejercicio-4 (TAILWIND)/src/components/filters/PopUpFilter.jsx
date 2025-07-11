01cfc55ac23289e7f0fced70cf14298cc7e620fb
matiasmoscone1 <matiasmoscone1@gmail.com>
2025-07-11 09:39:26 -0300

    Mejorando la estructura del renderizado de tareas

--- ejercicio-4 (TAILWIND)/src/components/filters/PopUpFilter.jsx
+++ ejercicio-4 (TAILWIND)/src/components/filters/PopUpFilter.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { useRef } from "react";
 import { TaskContext } from "../../context/TaskContext";
@@ -8,7 +9,7 @@ const PopUpFilter = ( ) => {
 
     const { state, dispatch } = useContext(TaskContext);
     const popUpFilterRef = useRef();
-    const [filterArray, setFilterArray] = ([]);
+    const [filterArray, setFilterArray] = useState([]);
 
     const closePopUpFilter = (e) => {
         if(popUpFilterRef.current && !popUpFilterRef.current.contains(e.target)){
@@ -17,19 +18,38 @@ const PopUpFilter = ( ) => {
     }
 
     useEffect(() => {
-        setFilterArray(state.tasks.filter((task) => {
-            if(state.filterState.ref === "Username"){
-                task.user === state.filterState.user;
-            }
-        }))
+        let newArray = [];
+        switch(state.filter.ref){
+            case "Usuario": 
+                newArray = state.tasks.filter((task) => task.user === state.filterState.user)
+                setFilterArray(newArray);
+                break;
+            case "Estado": 
+                newArray = state.tasks.filter((task) => task.user === state.filterState.user)
+                setFilterArray(newArray);
+                break;
+            case "Fecha": 
+                newArray = state.tasks.filter((task) => task.user === state.filterState.user)
+                setFilterArray(newArray);
+                break;
+            default:
+                break;
+        }
+
+        // if(state.filterState.ref === "Usuario"){
+        //     const newArray = state.tasks.filter((task) => task.user === state.filterState.user)
+        //     setFilterArray(newArray);
+        // }
     }, []);
+
+    console.log(filterArray);
 
     return(<div className="fixed inset-0 flex justify-center items-center bg-black/50 z-50" onClick={(e) => closePopUpFilter(e)}>
         <div className="w-7/10 h-9/10 bg-white border border-gray-500 rounded-md shadow-lg p-4 relative" ref={popUpFilterRef}>
             <span className="p-4 text-lg block font-semibold text-center">Tareas filtradas por {state.filterState.ref}</span>
             {filterArray.map((task) => {
                 return(<div>
-                    {task}
+                    {task.user}
                     </div>)
             })}
 
