9d663a6bae996c264dda9671f8b00e14297119e9
matiasmoscone1 <matiasmoscone1@gmail.com>
2025-07-15 15:14:18 -0300

    Mejorando popup de cerrar sesion

--- ejercicio-4 (TAILWIND)/src/components/UserProfile.jsx
+++ ejercicio-4 (TAILWIND)/src/components/UserProfile.jsx
@@ -23,10 +23,10 @@ const UserProfile = () => {
             <img src="./public/images/close-session.png" className="w-5 h-5 rotate-180 cursor-pointer hover:scale-125 duration-300 fill-white hover:drop-shadow" 
             onClick={() => dispatch({type: "FLAG_SESSION", payload: true})}></img>
         </div>
-        {state.flagSession && <div className="fixed inset-0 flex justify-center items-center bg-black/50 z-50" onClick={() => closePopUp(e)}>
-            <div className="w-1/3 h-1/3 bg-white rounded-md" ref={refPopUp}>
-                <span>¿Desea cerrar la sesión?</span>
-                <div>
+        {state.flagSession && <div className="fixed inset-0 flex justify-center items-center bg-black/50 z-50" onClick={(e) => closePopUp(e)}>
+            <div className="w-1/3 h-1/3 bg-white rounded-md flex flex-col justify-between items-center" ref={refPopUp}>
+                <span className="flex-1 flex justify-center items-center">¿Seguro desea cerrar la sesión?</span>
+                <div className="mb-4 flex justify-center items-center gap-4">
                     <button>Salir</button>
                     <button>Cancelar</button>
                 </div>
