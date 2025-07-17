596d7ac8a4476df2a120f362e96134f1dff71bad
matiasmoscone1 <matiasmoscone1@gmail.com>
2025-07-17 14:34:15 -0300

    Creando unos tooltip distintos para la lista de tareas globales porque no renderizaba correctamente con el overflow-y-auto el tooltip anterior

--- ejercicio-4 (TAILWIND)/src/components/tasks/GlobalTaskList.jsx
+++ ejercicio-4 (TAILWIND)/src/components/tasks/GlobalTaskList.jsx
@@ -1,16 +1,26 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { TaskContext } from "../../context/TaskContext";
 
 const GlobalTaskList = () => {
 
     const { state, dispatch } = useContext(TaskContext);
 
-    const handleMouseEnter = (e) => {
+    const [tooltipPosition, setTooltipPosition] = useState({ top: 0, left: 0 });
+    const [showTooltip, setShowTooltip] = useState(false);
+    const [tooltipText, setTooltipText] = useState("");
+  
+    const handleMouseEnter = (e, text) => {
         const rect = e.target.getBoundingClientRect();
         setTooltipPosition({
-          top: rect.bottom + window.scrollY, // para abajo del elemento
-          left: rect.left + window.scrollX,  // alineado a la izquierda del elemento
+          top: rect.bottom + window.scrollY + 5,
+          left: rect.left + window.scrollX - 500,
         });
+        setTooltipText(text);
+        setShowTooltip(true);
+      };
+    
+      const handleMouseLeave = () => {
+        setShowTooltip(false);
       };
 
     return(<div className="w-full max-h-[75vh] overflow-y-auto overflow-x-visible border rounded-sm p-4">
@@ -25,13 +35,22 @@ const GlobalTaskList = () => {
                     <span className="w-40 truncate" title={task.user}>{task.user}</span>
                     <span className="text-sm w-60 truncate group">{task.title}
                     <div className="w-100 h-10 justify-center items-center absolute top-5 left-50 border bg-white rounded-sm hidden group-hover:flex z-50">{task.title}</div></span>
-                    <span className="text-sm w-60 truncate group">{task.description}
-                    <div className="w-120 h-10 justify-center items-center absolute top-5 right-50 border bg-white rounded-sm hidden group-hover:flex z-50">{task.description}</div></span>
+                    <span className="text-sm w-60 truncate group" onMouseEnter={(e) => handleMouseEnter(e, task.description)}
+                onMouseLeave={handleMouseLeave}
+              >{task.description}</span>
                     <span>{task.date}</span>
                     <span>{task.state === "pendiente" ? <input type="checkbox" /> : <input type="checkbox" checked/> }</span>
                     
                 </div>)
             })}
+            {showTooltip && (
+        <div
+          className="absolute w-150 h-10 flex justify-center items-center bg-white border rounded-sm text-sm shadow-md z-50 break-words"
+          style={{ top: tooltipPosition.top, left: tooltipPosition.left, position: "absolute" }}
+        >
+          {tooltipText}
+        </div>
+      )}
         </div>
     </div>)
 
