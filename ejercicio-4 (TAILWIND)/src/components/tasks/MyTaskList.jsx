595df7a373aa95a91f4e3d7f8bd3251a49f368e9
matiasmoscone1 <matiasmoscone1@gmail.com>
2025-07-17 13:39:53 -0300

    Aprendiendo a hacer una tooltip con group y group-hover, ejercicio-4

--- ejercicio-4 (TAILWIND)/src/components/tasks/MyTaskList.jsx
+++ ejercicio-4 (TAILWIND)/src/components/tasks/MyTaskList.jsx
@@ -8,6 +8,7 @@ const MyTaskList = () => {
 
     const { state, dispatch } = useContext(TaskContext);    
     const [filterArray, setFilterArray] = useState([]);
+    const [signTooltip, setSignTooltip] = useState(false);
 
     useEffect(() => {
         setFilterArray(state.tasks.filter((task) => task.user === state.userLogged.user));        
@@ -23,7 +24,8 @@ const MyTaskList = () => {
             {filterArray.map((task) => {
                 return(<div key={task.id} className="p-2 flex justify-center items-center gap-2 border-b">
                     <span className="w-40 truncate" title={task.user}>{task.user}</span>
-                    <span className="text-sm w-40 truncate" title={task.title}>{task.title}</span>
+                    <span className="text-sm w-60 relative truncate group" title={task.title}>{task.title}
+                    <div className="w-100 h-20 absolute top-full border rounded-sm none group-hover:block z-50">{task.title}</div></span>
                     <span className="text-sm w-80 truncate" title={task.description}>{task.description}</span>
                     <span>{task.date}</span>
                     <span>{task.state === "pendiente" ? <input type="checkbox" /> : <input type="checkbox" checked/> }</span>
