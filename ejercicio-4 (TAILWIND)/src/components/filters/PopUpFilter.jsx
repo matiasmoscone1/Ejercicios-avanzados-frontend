ed6e9b844372eee6b5ad20b4c55f6879973d15e2
matiasmoscone1 <matiasmoscone1@gmail.com>
2025-07-17 15:47:55 -0300

    Mejorando el renderizado de filtros con useMemo

--- ejercicio-4 (TAILWIND)/src/components/filters/PopUpFilter.jsx
+++ ejercicio-4 (TAILWIND)/src/components/filters/PopUpFilter.jsx
@@ -10,35 +10,12 @@ const PopUpFilter = ( ) => {
 
     const { state, dispatch } = useContext(TaskContext);
     const popUpFilterRef = useRef();
-    //const [filterArray, setFilterArray] = useState([]);
-
+    
     const closePopUpFilter = (e) => {
         if(popUpFilterRef.current && !popUpFilterRef.current.contains(e.target)){
             dispatch({type: "POPUP_FILTER", payload: false});
         }
     }
-
-    /*
-    useEffect(() => {
-        let newArray = [];
-        switch(state.filterState.ref){
-            case "Usuario":
-                newArray = state.tasks.filter((task) => task.user === state.filterState.user);
-                setFilterArray(newArray);
-                break;
-            case "Estado":
-                newArray = state.tasks.filter((task) => state.filterState.state === (task.state === "completada" ? true : false));
-                setFilterArray(newArray);
-                break;
-            case "Fecha":
-                newArray = state.tasks.filter((task) => task.date === state.filterState.date.split("-").reverse().join("/"));
-                setFilterArray(newArray);
-                break;
-            default:
-                break;
-        }
-
-    }, [state.tasks, state.filterState]);*/
 
     const filterArray = useMemo(() => {
         switch(state.filterState.ref){
@@ -49,7 +26,7 @@ const PopUpFilter = ( ) => {
             case "Fecha":
                 return state.tasks.filter((task) => task.date === state.filterState.date.split("-").reverse().join("/"));
             default:
-                break;
+                return [];
         }
 
     }, [state.tasks, state.filterState]);
